635cbe26e12385330d57e339a256fae2d839fe47
Sawyer <35255629+shaoyie@users.noreply.github.com>
2022-11-09 20:17:24 +0800

    refactor: 暴露接口给 web service 发布功能

--- scripts/codegen/kick_off.py
+++ scripts/codegen/kick_off.py
@@ -5,10 +5,13 @@ from create_ticos_proj import generate as prj_generator
 
 PROJ_NAME = 'ticos_project'
 
+def generate(name, platform, thingmodel='', to='.'):
+    prj_generator(PROJ_NAME, args.platform, args.thingmodel, args.to)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='kick off')
     parser.add_argument('--platform', type=str, help='supported platform: arduino (default) | esp32')
     parser.add_argument('--thingmodel', type=str, help='json file|data of thing model')
     parser.add_argument('--to', type=str, default='.', help='target directory')
     args = parser.parse_args()
-    prj_generator(PROJ_NAME, args.platform, args.thingmodel, args.to)
+    generate(PROJ_NAME, args.platform, args.thingmodel, args.to)
