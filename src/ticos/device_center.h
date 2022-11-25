afec0cfdb64dce1b57c4f819d15f914793d14ecb
Masa <20962057+hypermasa@users.noreply.github.com>
2022-11-25 17:43:45 +0800

    refactor: 去除设备中心冗余接口

--- src/ticos/device_center.h
+++ src/ticos/device_center.h
@@ -36,42 +36,13 @@ int ticos_device_center_dump(void);
   * @param[in] id 设备 ID
   * @return 设备句柄
   */
-#define ticos_dev(id)    \
-   (id)
+#define ticos_dev(id)       (id)
 
 /**
   * @brief 通过设备 ID 检索 设备名
   * @return 设备名
   */
-#define ticos_dev_name(id)        \
-    (ticos_dev(id)->name)
-
-/**
-  * @brief 判断设备实例的 public 成员变量是否有刷新
-  * @param[in] id 设备 ID
-  * @param[in] v 成员变量，仅接受实例的 public 成员变量
-  * @return true：刷新，false：无刷新
-  */
-#define ticos_dev_isdirty(id, v)   \
-    ticos_isdirty(ticos_dev(id), v)
-
-/**
-  * @brief 获取设备实例的 public 成员变量值
-  * @param[in] id 设备 ID
-  * @param[in] v 成员变量，仅接受实例的 public 成员变量
-  * @return 返回变量值
-  */
-#define ticos_dev_getval(id, v)  \
-    ticos_get(ticos_dev(id), v)
-
-/**
-  * @brief 设置设备实例的 public 成员变量值
-  * @param[in] self 设备 ID
-  * @param[in] v 成员变量，仅接受实例的 public 成员变量
-  * @param[in] d 待更新的变量值输入
-  */
-#define ticos_dev_setval(id, v, d) \
-    ticos_set(ticos_dev(id), v, (d))
+#define ticos_dev_name(id)  (ticos_dev(id)->name)
 
 #ifdef __cplusplus
 }
